babd51f8d566d7de763a74d589ee2b6c09e965ce
r.chernetsov <r.chernetsov@adonis24.ru>
2019-02-06 13:44:51 +0300

    lesson-1

---
+++ VK_lesson_2/Авторизация/Session.swift
@@ -0,0 +1,21 @@
+//
+//  Session.swift
+//  VK_lesson_2
+//
+//  Created by Чернецова Юлия on 06/02/2019.
+//  Copyright © 2019 Чернецов Роман. All rights reserved.
+//
+
+import Foundation
+class Session{
+    var token: String
+    var userid: Int
+    init(token:String, userid:Int) {
+        self.token = token
+        self.userid = userid
+        
+    }
+  
+
+}
+
