0e4238476967623eabbde6b4ab3e9fe6cf845552
r.chernetsov <r.chernetsov@adonis24.ru>
2019-02-10 11:54:56 +0300

    add class

---
+++ VK_lesson_2/Авторизация/NetworkServices.swift
@@ -0,0 +1,104 @@
+//
+//  AlamofireServices.swift
+//  VK_lesson_2
+//
+//  Created by Чернецова Юлия on 09/02/2019.
+//  Copyright © 2019 Чернецов Роман. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+//Получение списка друзей;
+//Получение фотографий человека;
+//Получение групп текущего пользователя;
+//Получение групп по поисковому запросу;
+
+enum typeRequest {
+    
+    case getFriends
+    case getFotos
+    case getGroups
+    case getSearchGroups
+    
+}
+
+class NetWorkServices{
+    
+
+    public func sendRequest(type: typeRequest){
+        let url = "https://api.vk.com"
+        
+        switch type {
+        case .getFriends:
+            
+        let path = "/method/friends.get"
+        let parameters: Parameters = [
+            "access_token":Session.instance.token,
+            "order":"name",
+            "fields":"city"
+            ]
+        
+        Alamofire.request(url+path, method: .get, parameters:parameters)
+            .responseJSON{response in
+        guard let value = response.value else {return}
+        print(value)
+            }
+            
+        case .getFotos:
+            let path = "/method/photos.get"
+            let parameters: Parameters = [
+                "access_token":Session.instance.token,
+                "owner_id":"-1",
+                "album_id":"wall",
+                "count":"1"
+            ]
+            
+            Alamofire.request(url+path, method: .get, parameters:parameters)
+                .responseJSON{response in
+                    guard let value = response.value else {return}
+                    print(value)
+            }
+        case .getGroups: //groups.get
+            let path = "/method/groups.get"
+            let parameters: Parameters = [
+                "access_token":Session.instance.token,
+                "extended":"1",
+                "count":"1"
+            ]
+            
+            Alamofire.request(url+path, method: .get, parameters:parameters)
+                .responseJSON{response in
+                    guard let value = response.value else {return}
+                    print(value)}
+        case .getSearchGroups://groups.search
+                    let path = "/method/groups.search"
+                    let parameters: Parameters = [
+                        "q":""
+                    ]
+                    
+                    Alamofire.request(url+path, method: .get, parameters:parameters)
+                        .responseJSON{response in
+                            guard let value = response.value else {return}
+                            print(value)}
+            
+    }
+        
+    }
+    func getGroups(){
+        let url = "https://api.vk.com"
+        let path = "/method/groups.get"
+        let parameters: Parameters = [
+            "access_token":Session.instance.token,
+            "extended":"1",
+            "count":"1"
+        ]
+        
+        Alamofire.request(url+path, method: .get, parameters:parameters)
+            .responseJSON{response in
+                guard let value = response.value else {return}
+                print(value)}
+    }
+    
+    
+}
